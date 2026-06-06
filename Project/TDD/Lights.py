9d58a6bd8bc96fa2cc2937ca56a7e82addbabb8b
Bartosz Muzdzak <257196@edu.p.lodz.pl>
2026-06-06 17:40:10 +0200

    feat(GREEN): implement basic controller with safe starting state

--- Project/TDD/Lights.py
+++ Project/TDD/Lights.py
@@ -1,4 +1,4 @@
-# class TrafficLightController:
-#     def __init__(self):
-#         self.ns_light = "Green"
-#         self.ew_light = "Red"
+class TrafficLightController:
+    def __init__(self):
+        self.ns_light = "Green"
+        self.ew_light = "Red"
